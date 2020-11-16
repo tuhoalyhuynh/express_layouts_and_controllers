14b1ef66754a6c9dc3b0cbc7fd47af765aab789e
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-11-16 15:42:57 -0500

    [index.js] add express-ejs-layouts, changed index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // import stuff
 
 const express = require('express');
+// const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
+const ejsLayouts = require('express-ejs-layouts');
 
-app.set('view engines', 'ejs');
-
-
+app.set('view engine', 'ejs');
+// app.use(expressEjsLayouts);
+app.use(ejsLayouts)
 
 // create our routes
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index')
 })
 
 // app.listen
